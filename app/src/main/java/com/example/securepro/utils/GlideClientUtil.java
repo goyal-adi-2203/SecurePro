c22151947daa7fc57237be0d73c1f253ce888d03
Aditya Goyal <goyaladitya2212@gmail.com>
2024-10-15 01:38:00 +0530

    glide caching updation

--- app/src/main/java/com/example/securepro/utils/GlideClientUtil.java
+++ app/src/main/java/com/example/securepro/utils/GlideClientUtil.java
@@ -5,12 +5,13 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.securepro.BuildConfig;
 import com.example.securepro.R;
 
 public class GlideClientUtil {
-    private static String baseUrl = BuildConfig.BASE_URL2;
+    private static String baseUrl = BuildConfig.BASE_URL;
 //    private static String baseUrl = BuildConfig.BASE_URL3;
     private static Glide glide = null;
 
@@ -19,6 +20,8 @@ public class GlideClientUtil {
                 .load(baseUrl + path)
                 .apply(new RequestOptions()
                         .placeholder(R.drawable.default_pro_pic)
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
                 )
                 .into(view);
     }
